926fb64bc10dac9775f2111c8f44f6fe7e3f822c
Raphael Dussin <Raphael.Dussin@noaa.gov>
2024-07-02 10:47:33 -0400

    update velocities MAR

--- examples/remap_MAR_velocities.py
+++ examples/remap_MAR_velocities.py
@@ -1,8 +1,10 @@
 import xarray as xr
 from mom_in_stereo.MAR import remap_MAR_velocities_to_MOM6
-from mom_in_stereo.coords import add_lon_lat
+from mom_in_stereo.rotate_vector import rotate_MOM6_to_EW
+from mom_in_stereo.coords import add_lon_lat, MOM6_hgrid_to_xesmf
 import cartopy.crs as ccrs
 import matplotlib.pyplot as plt
+import numpy as np
 
 MAR = "/net2/Olga.Sergienko/MARv3.6.4-Antarctica/ERA-Interim/"
 MOMgrids = "/net2/rnd/Olga"
@@ -21,6 +23,7 @@ uvmar = xr.open_mfdataset(
 
 # load MOM6 grid
 hgrid = xr.open_dataset(f"{MOMgrids}/sosc73E82S_10rr.nc")
+hgrid = hgrid.set_coords(["x", "y"])
 
 # === Remap velocities
 
@@ -38,6 +41,7 @@ uvmar_remapped_coarse = (
     uvmar_remapped.isel(time=0, atmlay=0).coarsen(xh=10, yh=10).mean()
 )
 
+uvmar_remapped_coarse.load()
 
 # quiver plot original data
 plt.figure(figsize=[12, 8])
@@ -49,6 +53,7 @@ plt.quiver(uvmar_coarse["x"], uvmar_coarse["y"], uvmar_coarse["UU"], uvmar_coars
 plt.figure(figsize=[12, 8])
 spproj = ccrs.SouthPolarStereo(central_longitude=0.0)
 ax = plt.axes(projection=spproj)
+hgrid["angle_dx"].plot(ax=ax, x="x", y="y", cmap="hsv", transform=ccrs.PlateCarree() )
 uvmar_remapped_coarse.plot.quiver(
     "lon", "lat", "UU", "VV", ax=ax, transform=ccrs.PlateCarree()
 )
@@ -56,4 +61,56 @@ ax.coastlines()
 ax.set_extent([-180, 180, -50, -90], ccrs.PlateCarree())
 
 
+# MOM6 grid (rotated back to geo coordinates)
+ds_mom6 = MOM6_hgrid_to_xesmf(hgrid)
+ugeo, vgeo = rotate_MOM6_to_EW(uvmar_remapped["UU"], uvmar_remapped["VV"], ds_mom6["angle_dx"])
+geo= xr.Dataset()
+geo["UU"] = ugeo
+geo["VV"] = vgeo
+geo_coarse = geo.isel(time=0, atmlay=0).coarsen(xh=10, yh=10).mean()
+geo_coarse.load()
+
+plt.figure(figsize=[12, 8])
+spproj = ccrs.SouthPolarStereo(central_longitude=0.0)
+ax = plt.axes(projection=spproj)
+hgrid["angle_dx"].plot(ax=ax, x="x", y="y", cmap="hsv", transform=ccrs.PlateCarree() )
+geo_coarse.plot.quiver(
+    "lon", "lat", "UU", "VV", ax=ax, transform=ccrs.PlateCarree()
+)
+ax.coastlines()
+ax.set_extent([-180, 180, -50, -90], ccrs.PlateCarree())
+
+# check the difference between rotated and non-rotated
+
+diffu = uvmar_remapped_coarse["UU"] - geo_coarse["UU"]
+
+plt.figure(figsize=[12, 8])
+spproj = ccrs.SouthPolarStereo(central_longitude=0.0)
+ax = plt.axes(projection=spproj)
+hgrid["angle_dx"].plot.contour(ax=ax, x="x", y="y", levels=np.arange(-180,180, 60),
+                               transform=ccrs.PlateCarree() )
+diffu.plot(
+    x="lon", y="lat", ax=ax, transform=ccrs.PlateCarree()
+)
+ax.coastlines()
+ax.set_extent([-180, 180, -50, -90], ccrs.PlateCarree())
+
+
+plt.figure(figsize=[12, 8])
+
+# Plot in South Polar Stereographic projection
+ax = plt.axes(projection = ccrs.SouthPolarStereo())
+
+# Set the extent of the plot
+ax.set_extent([-180, 180, -90, -65], crs = ccrs.PlateCarree())
+
+# Plot Antartic coastline for reference
+ax.coastlines()
+
+# Plot the arrows
+ax.quiver(geo_coarse["lon"].values, geo_coarse["lat"].values,
+          geo_coarse["UU"].values, geo_coarse["VV"].values,
+          transform = ccrs.PlateCarree(), angles = "xy")
+
 plt.show()
+
